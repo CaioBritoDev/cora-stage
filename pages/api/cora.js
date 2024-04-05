da353ff8d77907569abdf540971f3672e3708747
Caio Brito <caio.freitas.brito@gmail.com>
2024-04-04 23:34:03 -0300

    Updated Version

--- pages/api/cora.js
+++ pages/api/cora.js
@@ -19,11 +19,11 @@ function coraToken(request, response) {
 
   // C:\Users\caiof\OneDrive\Documentos\cora-stage\pages\api
 
-  const certPath = path.join(process.cwd(), '\\pages\\api\\certificate.pem');
+  const certPath = path.join(process.cwd(), 'pages/api/certificate.pem');
 
   const cert = fs.readFileSync(certPath);
 
-  const keyPath = path.join(process.cwd(), '\\pages\\api\\private-key.key');
+  const keyPath = path.join(process.cwd(), 'pages/api/private-key.key');
 
   const key = fs.readFileSync(keyPath);
 
