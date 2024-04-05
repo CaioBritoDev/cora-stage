be5be8df66f7454fd5af25315b4780e9a93d9a06
Caio Brito <caio.freitas.brito@gmail.com>
2024-04-04 23:57:51 -0300

    Updated Version

---
+++ next.config.js
@@ -0,0 +1,6 @@
+// module.exports = {
+//   experimental: {
+//     outputFileTracingRoot: path.join(process.cwd(), 'certificate.pem'),
+//     outputFileTracingRoot: path.join(process.cwd(), 'private-key.key')
+//   },
+// }
